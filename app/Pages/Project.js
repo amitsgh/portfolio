40211e4015e8149266bdfefdd7a973ec7e04e31c
amitsgh <amitsingh1911nov@gmail.com>
2024-06-24 13:04:28 +0530

    ⚰️ Removed dead code

--- app/Pages/Project.js
+++
@@ -1,8 +0,0 @@
-export default function ProjectPage() {
-	return (
-		<section className="flex animate-in flex-col gap-8">
-			<p className="tracking-tight text-secondary">Pinned Projects</p>
-			{/* <ProjectList projects={projects} /> */}
-		</section>
-	);
-}

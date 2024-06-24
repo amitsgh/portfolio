051b4d20c3036274e160ee5da76db31a102c894c
amitsgh <amitsingh1911nov@gmail.com>
2024-06-24 12:58:12 +0530

    💄 Added tailwindcss configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: ["class"],
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-			textColor: {
-				primary: "var(--gray-12)",
-				secondary: "var(--gray-11)",
-				tertiary: "var(--gray-9)",
-				link: "var(--blue-11)",
-			},
-			fontFamily: {
-				sans: ["var(--font-neue-montreal)", "system-ui", "sans-serif"],
-				mono: ["var(--font-inter)", "monospace"],
-			},
-			backgroundColor: {
-				contrast: "var(--contrast-color)",
-				primary: "var(--gray-1)",
-				secondary: "var(--gray-2)",
-				tertiary: "var(--gray-3)",
-			},
-		},
-	},
-	plugins: [],
+    darkMode: ["class"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            borderColor: {
+                primary: "var(--gray-12)",
+                secondary: "var(--gray-11)",
+                tertiary: "var(--gray-9)",
+            },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                "gradinet-4":
+                    "linear-gradient(to right, var(--red-7), var(--blue-7), var(--yellow-7), var(--green-7)) 1",
+            },
+            textColor: {
+                primary: "var(--gray-12)",
+                secondary: "var(--gray-11)",
+                tertiary: "var(--gray-9)",
+            },
+            fontFamily: {
+                sans: ["var(--font-neue-montreal)", "system-ui", "sans-serif"],
+                mono: ["var(--font-inter)", "monospace"],
+            },
+            backgroundColor: {
+                contrast: "var(--contrast-color)",
+                primary: "var(--gray-1)",
+                secondary: "var(--gray-3)",
+                tertiary: "var(--gray-5)",
+            },
+        },
+    },
+    plugins: [
+        function ({ addUtilities }) {
+            const newUtilities = {
+                ".border-gradient": {
+                    borderImage: "var(--border-image)",
+                },
+            };
+            addUtilities(newUtilities);
+        },
+    ],
 };
